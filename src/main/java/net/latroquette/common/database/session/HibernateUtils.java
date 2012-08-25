d754276a8177dc55c0ea4bc54477c678a2d739d4
adi <adi3000@free.fr>
2012-08-25 13:38:00 +0200

    #44 Issue with deprecated buildSessionFactory. Cannot use for now buildSesssionFactory(ServiceFactory sf) without error.

--- src/main/java/net/latroquette/common/database/session/HibernateUtils.java
+++ src/main/java/net/latroquette/common/database/session/HibernateUtils.java
@@ -1,25 +1,29 @@
 package net.latroquette.common.database.session;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtils {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
-
-    private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
-        }
-        catch (Throwable ex) {
+    private static final String HIBERNATE_CFG_PATH = "/WEB-INF/hibernate.cfg.xml";
+    private static SessionFactory buildSessionFactory() { 
+    	// Create the SessionFactory from hibernate.cfg.xml
+    	//ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder();
+    	Configuration config = new Configuration();
+    	try{
+    		return  config
+    				.configure()
+    				.buildSessionFactory();
+    	}catch (HibernateException ex) {
             // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            System.err.println("Initial SessionFactory creation failed. Unable to found " +HIBERNATE_CFG_PATH + ex);
             throw new ExceptionInInitializerError(ex);
         }
+        
     }
-
-    
     
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
