59dc3fe4ecb10ffe958c5ea849ea267ec181d549
adi <adi3000@free.fr>
2012-09-01 17:25:15 +0200

    #49 Ajout des fonctionnalités JAXB pour JSON

---
+++ src/main/java/net/latroquette/rest/search/LocationSearch.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package net.latroquette.rest.search;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+import net.latroquette.common.database.data.location.Location;
+import net.latroquette.common.database.data.location.Locations;
+
+/**
+ * @author adi
+ *
+ */
+@Path("/search/locations")
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class LocationSearch {
+	
+	
+	@GET
+	public GenericEntity<List<Location>> getLocations (@QueryParam("q") String pattern){
+		Locations locations = new Locations();
+		List<Location> locationsFound = locations.getLocationByString(pattern);
+		return new GenericEntity<List<Location>>(locationsFound) {};
+	}
+}
