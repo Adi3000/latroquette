d05a99595441eac4ac4f16cee5e55b28e57e8324
adi <adi3000@free.fr>
2013-04-30 13:14:38 +0200

    Renamed AbstractBusinessObject to AbstractDAO Removed useless hbm.xml

---
+++ src/main/java/net/latroquette/common/database/data/AbstractDAO.java
@@ -0,0 +1,8 @@
+package net.latroquette.common.database.data;
+
+import net.latroquette.common.database.session.DatabaseSession;
+
+public abstract class AbstractDAO extends DatabaseSession{
+	
+	
+}
