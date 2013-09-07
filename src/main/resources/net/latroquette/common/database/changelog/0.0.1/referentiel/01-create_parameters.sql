43eed751b96f70609bff8c4ac9ef59899b959ead
adi <adi3000@free.fr>
2013-09-07 16:38:25 +0200

    Missing NB_RESULT_TO_LOAD parameter

---
+++ src/main/resources/net/latroquette/common/database/changelog/0.0.1/referentiel/01-create_parameters.sql
@@ -0,0 +1,6 @@
+--
+-- Data for Name: parameters; Type: TABLE DATA; Schema: public; Owner: latroquette
+--
+
+INSERT INTO parameters (param_id, param_name, param_value, param_data_type, param_description) VALUES
+(4,	'NB_RESULT_TO_LOAD',	'10',	'n1',	'Item result per page loaded');
