4f4e51589e7ac022f5a7253132c429bd08e6c97f
adi <adi3000@free.fr>
2013-11-24 23:25:03 +0100

    ref #71 add check user web service for ejabberd

--- src/main/java/net/latroquette/rest/security/Authentication.java
+++ src/main/java/net/latroquette/rest/security/Authentication.java
@@ -1,10 +1,14 @@
 package net.latroquette.rest.security;
 
+import java.util.List;
+
 import javax.jws.WebService;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 
 import net.latroquette.common.database.data.profile.UsersService;
@@ -14,7 +18,7 @@ import net.latroquette.web.security.AuthenticationMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import com.adi3000.common.util.security.User;
+import net.latroquette.common.database.data.profile.User;
 
 @Path("/authentication")
 @WebService(name=Services.AUTHENTICATION_WEB_SERVICE)
@@ -33,8 +37,25 @@ public class Authentication extends SpringBeanAutowiringSupport{
 	@POST
 	@Path("/ejabberd")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getClichedMessage(@QueryParam("login") String login, @QueryParam("password") String password) {
+    public String authenticateForEjabberd(@QueryParam("login") String login, @QueryParam("password") String password) {
 		User user = usersService.authenticateUser(login, password, AuthenticationMethod.EJABBER);
 		return user  == null ? "0" : "1";
     }
+	
+	@GET
+	@Path("/searchUser")
+	@Produces(MediaType.APPLICATION_JSON)
+	public GenericEntity<List<User>> searchUser(@QueryParam("q") String pattern){
+		List<User> usersFound = usersService.searchUsers(pattern);
+		return new GenericEntity<List<User>>(usersFound) {};
+	}
+	@POST
+	@Path("/isUser")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String isUser(@QueryParam("login") String login){
+		User user = usersService.getUserByLogin(login);
+		return user  == null ? "0" : "1";
+	}
+	
+	
 }
